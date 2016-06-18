7a9efd6a83c36115a6f37571136c6678b016b781
levanmanh <le.van.manh@framgia.com>
2016-06-18 20:49:41 +0700

    view-edit-delete-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user
+  before_action :load_user, only: [:show, :update, :edit]
 
   def new
     @user = User.new
@@ -8,10 +10,24 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       log_in @user
-      flash[:success] = t "layouts.success_message"
+      flash[:success] = t "user.create_success"
       redirect_to root_path
     else
+      flash[:danger] = t "user.create_error"
       render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "user.update_success"
+      redirect_to @user
+    else
+      flash[:danger] = t "user.update_error"
+      render :edit
     end
   end
 
@@ -19,5 +35,9 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def load_user
+    @user = User.find params[:id]
   end
 end
